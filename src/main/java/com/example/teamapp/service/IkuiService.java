37f0b7f6fcf94e24203c924221f243c21f7f176d
3030736 <3030736@30307-36>
2026-06-09 11:43:25 +0900

    test03

--- src/main/java/com/example/teamapp/service/IkuiService.java
+++ src/main/java/com/example/teamapp/service/IkuiService.java
@@ -2,6 +2,7 @@ package com.example.teamapp.service;
 import org.springframework.stereotype.Service;
 
 @Service
+
 public class  IkuiService {
     public String getProfile() {
         return "メンバーの生井です！趣味は趣味はドライブです。";
