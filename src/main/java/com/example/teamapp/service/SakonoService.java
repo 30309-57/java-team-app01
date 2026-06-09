5ff164be53a4be9fe8ea7f1f59cb6927d071f521
3030717 <3030717@30307-17>
2026-06-09 10:11:34 +0900

    sakono1

---
+++ src/main/java/com/example/teamapp/service/SakonoService.java
@@ -0,0 +1,11 @@
+package com.example.teamapp.service;
+
+	import org.springframework.stereotype.Service;
+
+	@Service
+	public class SakonoService {
+	    public String getProfile() {
+	        return "メンバーの迫野です！趣味は映画鑑賞です。";
+	    
+	}
+}
