ec487e278f4ec9ed41a7444c6bf89b406553d157
3030957 <3030957@host.docker.internal>
2026-06-09 09:18:13 +0900

    First Commit (枠組み作成)

---
+++ src/main/java/com/example/teamapp/JavaTeamApp01Application.java
@@ -0,0 +1,13 @@
+package com.example.teamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTeamApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTeamApp01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teamapp/JavaTeamApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTeamApp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
