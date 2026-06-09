86d0df7f09370f40c401bc5f58b9590ba1b89bae
3030736 <3030736@30307-36>
2026-06-09 11:06:11 +0900

    a

---
+++ src/main/java/com/example/teamapp/service/IkuiService.java
@@ -0,0 +1,9 @@
+package com.example.teamapp.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class  IkuiService {
+    public String getProfile() {
+        return "メンバーの生井です！趣味は趣味はドライブです。";
+    }
+}
